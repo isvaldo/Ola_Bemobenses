5905ce02d503509a379b85a2906eb58087df06ac
= <isvaldo.fernandes@gmail.com>
2015-12-09 20:04:31 -0200

    editando readme

---
+++ js/facebook.js
@@ -0,0 +1,276 @@
+/**
+ * created by isvaldo on 04/12/15.
+ */
+var data = {
+    "data": [
+        {
+            "name": "luciene marques",
+            "id": "920820974670080"
+        },
+        {
+            "name": "adilio rosa",
+            "id": "830791526978604"
+        },
+        {
+            "name": "ariel faria",
+            "id": "914382678648056"
+        },
+        {
+            "name": "isvaldo fernandes souza",
+            "id": "830915886928163"
+        },
+        {
+            "name": "gill ferrari",
+            "id": "1641294466108976"
+        },
+        {
+            "name": "eliseu ferreira",
+            "id": "966197926730796"
+        },
+        {
+            "name": "gustavo da cruz",
+            "id": "819460634740155"
+        },
+        {
+            "name": "eric lopes",
+            "id": "799943670030581"
+        },
+        {
+            "name": "andre luiz xavier",
+            "id": "819967274695214"
+        },
+        {
+            "name": "vivian baumann",
+            "id": "1572475069683446"
+        },
+        {
+            "name": "victor meireles",
+            "id": "632792656797008"
+        },
+        {
+            "name": "guilherme facchinetti",
+            "id": "860982030611388"
+        },
+        {
+            "name": "luanda dandara",
+            "id": "10152230996285205"
+        },
+        {
+            "name": "fernando soliva",
+            "id": "1396420760685270"
+        },
+        {
+            "name": "diego marins",
+            "id": "867196006632748"
+        },
+        {
+            "name": "carmen lucia da sulva santos santos",
+            "id": "1559216110965788"
+        },
+        {
+            "name": "rodrigo luiz",
+            "id": "853014548114547"
+        },
+        {
+            "name": "raphael alves",
+            "id": "10205617041493162"
+        },
+        {
+            "name": "thais scarlet sobrinho",
+            "id": "10204786485997171"
+        },
+        {
+            "name": "alessandro santos",
+            "id": "867008586661245"
+        },
+        {
+            "name": "mariana variz salermo",
+            "id": "10204851343554945"
+        },
+        {
+            "name": "amanda vieira",
+            "id": "1158956710799477"
+        },
+        {
+            "name": "tainah torres",
+            "id": "724335394301685"
+        },
+        {
+            "name": "sarah moraes",
+            "id": "10205236158246437"
+        },
+        {
+            "name": "letícia martha",
+            "id": "10203486956874381"
+        },
+        {
+            "name": "flávia garro",
+            "id": "713658495401554"
+        },
+        {
+            "name": "igor rehem",
+            "id": "1055120074502863"
+        },
+        {
+            "name": "lucas marques",
+            "id": "651475278297946"
+        },
+        {
+            "name": "marcelo barbosa",
+            "id": "766883486712437"
+        },
+        {
+            "name": "diego silva",
+            "id": "1215109158504933"
+        },
+        {
+            "name": "caue nunes",
+            "id": "779036808844403"
+        },
+        {
+            "name": "israel mesquita",
+            "id": "4981883360457"
+        },
+        {
+            "name": "gabriel vilanova",
+            "id": "10204610073305051"
+        },
+        {
+            "name": "gabriel marcelino",
+            "id": "430444187103501"
+        },
+        {
+            "name": "isabela sampaio",
+            "id": "828073123892405"
+        },
+        {
+            "name": "douglas almeida",
+            "id": "833066643451986"
+        },
+        {
+            "name": "bruna freitas",
+            "id": "743232152440222"
+        },
+        {
+            "name": "jerssica arruda",
+            "id": "996564963705720"
+        },
+        {
+            "name": "mario soledade",
+            "id": "152114078482026"
+        },
+        {
+            "name": "diego azeredo",
+            "id": "774114989312319"
+        },
+        {
+            "name": "thays mesquita",
+            "id": "10203189433004634"
+        },
+        {
+            "name": "adriane casini",
+            "id": "589219394506549"
+        },
+        {
+            "name": "íris carlota",
+            "id": "828972047121702"
+        },
+        {
+            "name": "rhebeca macedo",
+            "id": "709330309149969"
+        },
+        {
+            "name": "leandro greijal",
+            "id": "873648229348325"
+        },
+        {
+            "name": "reinaldo benício",
+            "id": "734707679908798"
+        },
+        {
+            "name": "jean felipe braga",
+            "id": "710394922362812"
+        },
+        {
+            "name": "marcelo seroa",
+            "id": "797119846977825"
+        },
+        {
+            "name": "roberta oliveira",
+            "id": "666009656805580"
+        },
+        {
+            "name": "rafael ferreira",
+            "id": "814207388669254"
+        },
+        {
+            "name": "gioconda souza",
+            "id": "775122762562950"
+        },
+        {
+            "name": "gabriel bulhões",
+            "id": "932945943425048"
+        },
+        {
+            "name": "klayton santos",
+            "id": "716633955067880"
+        },
+        {
+            "name": "ricardo martins",
+            "id": "10203529538910290"
+        },
+        {
+            "name": "rafael stirling",
+            "id": "680329115378614"
+        },
+        {
+            "name": "diego gomes magalhães",
+            "id": "10153057825718455"
+        },
+        {
+            "name": "fernanda carvalho",
+            "id": "10204359632739529"
+        },
+        {
+            "name": "christiane sardinha",
+            "id": "10153355320914381"
+        },
+        {
+            "name": "fernanda leite",
+            "id": "1064354330264892"
+        },
+        {
+            "name": "karina ramalho m rigaud",
+            "id": "1043872415641798"
+        },
+        {
+            "name": "helena rodrigues",
+            "id": "943961188964480"
+        },
+        {
+            "name": "scarlett suzan",
+            "id": "862130603857219"
+        },
+        {
+            "name": "suzan scarlett",
+            "id": "862130603857219"
+        },
+        {
+            "name": "mario bello",
+            "id": "861433060570413"
+        },
+        {
+            "name": "alexandro lopes",
+            "id": "790128404345154"
+        },
+        {
+            "name":"leonardo gomes"
+        },
+        {
+            "name":"felipe oliveira"
+        },
+        {
+            "name":"marcus vinícius"
+        }
+    ]
+};
